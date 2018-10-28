09c499f7a99425e48438f1f98dfc5737582ce77c
Hanif Carroll <hanifcarroll@gmail.com>
2018-10-27 21:04:33 -0400

    Add get post, get posts, edit post, and delete post endpoints.

--- src/main/java/com/hanifcarroll/blog/post/PostController.java
+++ src/main/java/com/hanifcarroll/blog/post/PostController.java
@@ -1,7 +1,11 @@
 package com.hanifcarroll.blog.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -19,4 +23,49 @@ public class PostController {
         postRepository.save(post);
         return post;
     }
+
+    @GetMapping("/")
+    public List<Post> getPosts() {
+        return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getPost(@PathVariable("id") Long id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post
+                .map(foundPost -> ResponseEntity.ok().body(foundPost))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity editPost(@PathVariable("id") Long id,
+                         @RequestParam("title") String title,
+                         @RequestParam("body") String body
+    ) {
+        Optional<Post> post = postRepository.findById(id);
+
+        if (!post.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Post foundPost = post.get();
+        foundPost.setTitle(title);
+        foundPost.setBody(body);
+        postRepository.save(foundPost);
+
+        return ResponseEntity.ok(foundPost);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletePost(@PathVariable("id") Long id) {
+        Optional<Post> post = postRepository.findById(id);
+
+        if (!post.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        postRepository.deleteById(id);
+        return ResponseEntity.ok("Post deleted");
+    }
+
 }
